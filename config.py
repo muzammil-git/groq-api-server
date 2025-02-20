cccea87d6e0774f8c9dae9e7622978344c49b16b
muzammil-git <syedmuzammil67@gmail.com>
2025-02-21 04:10:45 +0500

    updated config.

--- config.py
+++ config.py
@@ -3,12 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 GROQ_KEY = os.getenv('GROQ')
 SERPER_API_KEY = os.getenv('SERPER')
 
-# DB_HOST = os.getenv('DB_HOST')
-# DB_PORT = os.getenv('DB_PORT')
-# DB_USER = os.getenv('DB_USER')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-# DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_USER = os.getenv('USERNAME')
+DB_PASSWORD = os.getenv('PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
--- database.py
+++ database.py
@@ -1,16 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 
-# [prod] : Will move to .env
-HOST = 'localhost'
-PORT = 5432
-USERNAME = 'postgres'
-PASSWORD = 'postgres'
-DATABASE = 'test_db'
 
 # postgresql://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 
